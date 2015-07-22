3f1b17e84b8f796efafaa5cb054e23a3bd5ac9c0
denisreis <dpcr@cin.ufpr.br>
2015-07-22 17:14:02 -0300

    pull

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,7 +20,7 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello World -  - Denis Teste!");
+    get("/hello", (req, res) -> "Hello World -  - Teste Pull Request!");
 
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
